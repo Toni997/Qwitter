ee52e2e1537c5d9c7911182705db60c4898eddea
Toni Kazinoti <toni.kazinoti@gmail.com>
2021-06-28 06:37:28 +0200

    single qweet design done

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -31,7 +31,39 @@
         />
       </div>
     </div>
+
     <q-separator class="divider" size="10px" color="grey-2" />
+
+    <q-list>
+      <q-item class="q-py-md">
+        <q-item-section avatar top>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/img/avatar2.jpg" />
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label class="text-subtitle1">
+            <span class="text-weight-bold"> Danny Connell </span>
+            <span class="text-grey-7"> @danny__connell </span>
+          </q-item-label>
+          <q-item-label class="qweet-content text-body1">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio
+            itaque reprehenderit eum consequun tur expedita cumque deserunt
+            adipisci architecto maiores, modi dolorem, dolorum cupiditate odio
+            non aperiam, ad nostrum labor um reiciendis.
+          </q-item-label>
+          <div class="qweet-icons row justify-between">
+            <q-btn flat round color="grey" icon="far fa-comment" size="sm" />
+            <q-btn flat round color="grey" icon="fas fa-retweet" size="sm" />
+            <q-btn flat round color="grey" icon="far fa-heart" size="sm" />
+            <q-btn flat round color="grey" icon="fas fa-trash" size="sm" />
+          </div>
+        </q-item-section>
+
+        <q-item-section side top> 1 min ago </q-item-section>
+      </q-item>
+    </q-list>
   </q-page>
 </template>
 
@@ -56,4 +88,8 @@ export default defineComponent({
   border-top: 1px solid
   border-bottom: 1px solid
   border-color: $grey-4
+.qweet-content
+  white-space: pre-line
+.qweet-icons
+  margin-left: -5px
 </style>
